81a881203c23b504fa581d5df54d25c9b0e83c70
Allan Niles <admin@allancoding.ga>
2022-11-10 09:32:45 -0700

    finish

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 enum RadioMessage {
-    message1 = 49434,
+    left = 14947,
+    right = 32391,
+    center = 31400,
     go = 39728,
-    stop = 61268,
-    start = 56380
+    message1 = 49434,
+    start = 56380,
+    stop = 61268
 }
 buttonClicks.onButtonDown(buttonClicks.AorB.B, function () {
-	
+    if (rcar == true) {
+        go()
+    }
 })
 radio.onReceivedMessage(RadioMessage.start, function () {
     start = false
@@ -17,10 +22,16 @@ radio.onReceivedMessage(RadioMessage.start, function () {
         . . . . .
         `)
     rcar = true
-    strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 })
 function stop () {
-    pins.digitalWritePin(DigitalPin.P2, 0)
+    if (car == true) {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showIcon(IconNames.Square)
+        goo = false
+        stopp = true
+    } else if (rcar == true) {
+        radio.sendMessage(RadioMessage.stop)
+    }
 }
 input.onButtonPressed(Button.A, function () {
     if (car == true) {
@@ -28,25 +39,75 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 buttonClicks.onButtonUp(buttonClicks.AorB.B, function () {
-	
+    if (rcar == true) {
+        stop()
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    if (rcar == true) {
+        radio.sendMessage(RadioMessage.left)
+    }
 })
 buttonClicks.onButtonUp(buttonClicks.AorB.A, function () {
-	
+    if (rcar == true) {
+        stop()
+    }
+})
+radio.onReceivedMessage(RadioMessage.go, function () {
+    if (car == true) {
+        go()
+    }
+})
+radio.onReceivedMessage(RadioMessage.left, function () {
+    if (car == true) {
+        basic.showArrow(ArrowNames.NorthWest)
+        servos.P1.setAngle(35)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.pause(1500)
+        basic.showIcon(IconNames.Square)
+    }
 })
 buttonClicks.onButtonDown(buttonClicks.AorB.A, function () {
-	
+    if (rcar == true) {
+        go()
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     if (start == true) {
         radio.sendMessage(RadioMessage.message1)
     }
-    if (car == true) {
-        servos.P1.setAngle(90)
+    if (rcar == true) {
+        radio.sendMessage(RadioMessage.center)
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (car == true) {
         stop()
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    if (rcar == true) {
+        radio.sendMessage(RadioMessage.right)
+    }
+})
+radio.onReceivedMessage(RadioMessage.stop, function () {
+    if (car == true) {
+        stop()
+    }
+})
+radio.onReceivedMessage(RadioMessage.right, function () {
+    if (car == true) {
+        basic.showArrow(ArrowNames.NorthEast)
+        servos.P1.setAngle(125)
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        basic.pause(1500)
+        basic.showIcon(IconNames.Square)
+    }
+})
+radio.onReceivedMessage(RadioMessage.center, function () {
+    if (car == true) {
+        servos.P1.setAngle(90)
+        basic.showIcon(IconNames.Square)
     }
 })
 radio.onReceivedMessage(RadioMessage.message1, function () {
@@ -60,8 +121,22 @@ radio.onReceivedMessage(RadioMessage.message1, function () {
         # # . # #
         `)
     car = true
+    servos.P1.setRange(20, 160)
+    strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 })
+function go () {
+    if (car == true) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.showArrow(ArrowNames.North)
+        goo = true
+        stopp = false
+    } else if (rcar == true) {
+        radio.sendMessage(RadioMessage.go)
+    }
+}
 let strip: neopixel.Strip = null
+let goo = false
+let stopp = false
 let car = false
 let rcar = false
 let start = false
@@ -70,6 +145,8 @@ radio.setTransmitPower(7)
 start = true
 rcar = false
 car = false
+stopp = true
+goo = false
 basic.showLeds(`
     . . . . .
     . # # # .
@@ -77,9 +154,18 @@ basic.showLeds(`
     # # . # #
     # # . # #
     `)
-servos.P1.setRange(20, 160)
 basic.forever(function () {
     if (start == true) {
         radio.sendNumber(0)
+    }
+    if (car == true) {
+        if (stopp == true) {
+            strip.showRainbow(1, 360)
+        }
+        if (goo == true) {
+            strip.rotate(1)
+            basic.pause(80)
+        }
+        strip.show()
     }
 })
